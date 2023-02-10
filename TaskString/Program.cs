9289a98df889e2295649f139752cfd28300e62e2
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-10 23:06:59 +0300

    Перевод символа в строку, а из строки в число

---
+++ TaskString/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Перевод символа в строку, а из строки в число.
+String(char) (Строка(символ))
+Нельзя путать типы данных "String" и "Char".
+"?" после "string" избавляет от занесения в поле значения "null".
+stroka[0] - символ строки.
+(stroka[0].ToString()) - строка.
+(Convert.ToInt32(stroka[0].ToString()) - число.*/
+
+string? stroka = Console.ReadLine();
+Console.WriteLine(Convert.ToInt32(stroka[0].ToString()) * 10);
