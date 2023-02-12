15101a73e19c40ae84cd8fe4b404f44995e9cfba
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-12 23:16:39 +0300

    Программа для поиска количества чисел в ведённом числе

---
+++ Task26/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа для поиска количества чисел в ведённом числе.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+while (n > 0)
+{
+n = n / 10;
+count++;
+}
+Console.WriteLine(count);
