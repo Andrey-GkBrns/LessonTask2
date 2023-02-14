08be493d058b9ed8e277f745c9362b1e5e9aa640
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-14 09:14:31 +0300

    Программа по поиску факториала числа от 1 до X

---
+++ Task28/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа по поиску факториала числа от 1 до X.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int X = Convert.ToInt32(Console.ReadLine()), count = 1;
+for (int i = 2; i <= X; i++)
+count = count * i;
+
+Console.WriteLine(count);
