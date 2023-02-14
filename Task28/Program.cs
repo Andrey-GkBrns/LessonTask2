6da2167a178a707e0c7b387ff5825475768213fe
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-14 16:18:32 +0300

    Программа для выведения элементов массива, с 0-ми и 1-ми в случайном порядке

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -5,5 +5,4 @@ Console.Write("Введите число: ");
 int X = Convert.ToInt32(Console.ReadLine()), count = 1;
 for (int i = 2; i <= X; i++)
 count = count * i;
-
 Console.WriteLine(count);
---
+++ Task30/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа для выведения элементов массива, с 0-ми и 1-ми в случайном порядке.
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int X = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[X];
+for (int Y = 0; Y < array.Length; Y++)
+array[Y] = new Random().Next(0, 2);
+Console.Write($"Результат: [{string.Join(", ", array)}]");
