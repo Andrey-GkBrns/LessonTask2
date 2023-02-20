ab7c7d736c2bd6e396e70c45e1cbccfa8ffeb235
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-20 17:47:08 +0300

    Программа для подсчёта до введённого числа методом Фибоначи

---
+++ Task44/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа для подсчёта до введённого числа методом Фибоначи.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a0 = 0, a1 = 1, x;
+for (int i = 0; i < n; i++)
+{
+Console.Write($"{a0} ");
+x = a0 + a1;
+a0 = a1;
+a1 = x;
+}
