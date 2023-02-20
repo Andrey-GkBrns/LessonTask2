0cf40b5fc4ea7e04cc56720bd3e3c1c2e41ea74a
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-20 17:11:26 +0300

    Программа для перевода числа из десятичной системы в двоичную

---
+++ Task42/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа для перевода числа из десятичной системы в двоичную.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = String.Empty;
+while (n > 0)
+{
+result = result + Convert.ToString(n % 2); // При данной записи порядок отсчёта нулей и единиц как раз начинается с конца.
+n = n / 2;
+}
+Console.WriteLine(result);
