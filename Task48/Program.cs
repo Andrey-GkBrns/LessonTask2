b567a52e9462eab8cd3855ef10bcb721a2fccc16
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-21 20:15:15 +0300

    Программа для создания двумерного массива посредством ввода заданных чисел, по формуле Amn = m + n

---
+++ Task48/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Программа для создания двумерного массива посредством ввода заданных чисел,
+где каждый элемент в массиве находится по формуле - Amn = m + n. */
+
+void InputMatrix(int[,] matrix)
+{for (int i = 0; i < matrix.GetLength(0); i++)
+{for (int j = 0; j < matrix.GetLength(1); j++)
+{matrix[i, j] = i + j;  /* Всё идентично 46-ой задаче, меняется лишь конечная
+часть этой строки после =. */
+Console.Write($"{matrix[i, j]} \t");}
+Console.WriteLine();}}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
