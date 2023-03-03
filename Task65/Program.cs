60c830901ec394e7240d8c1fff8141b47a4bff8a
Andrey-GkBrns <Oran@Gutang.ru>
2023-03-03 08:33:26 +0300

    Программа (рекурсия) для нахождения натуральных чисел в указанном промежутке

---
+++ Task65/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа (рекурсия) для нахождения натуральных чисел в указанном промежутке.
+
+string f(int m, int n)
+{
+if (n == m)
+return $"{m}";
+return f(m, n - 1) + $", {n}";
+}
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(m, n));
